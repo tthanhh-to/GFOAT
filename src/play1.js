c98c3b8e6cb8d7b269ea76ea9b163f56aa7f71ba
Thanh <thtto@ucsc.edu>
2023-06-04 00:04:19 -0700

    focus imported

--- src/play1.js
+++ src/play1.js
@@ -6,34 +6,25 @@ class play1 extends Phaser.Scene{
     preload(){
         this.load.path='./assets/';
         this.load.image('X','X.png');  
+        this.load.image('focus','focus.png');  
         this.load.image('tilesetImage','seven_eleven_tileset.png');
         this.load.tilemapTiledJSON('tilemapJSON','area01.json');
     }
     create(){
+//        this.player=this.add.container(38,35,[this.physics.add.sprite(38,35,'X',0).setScale(.1),])
         const map=this.add.tilemap('tilemapJSON');
         const seven_eleven_tileset= map.addTilesetImage('seven_eleven_tileset', 'tilesetImage');
 
         //add back
         const floorLayer=map.createLayer('floor',seven_eleven_tileset,0,0);
         const wallLayer=map.createLayer('walls',seven_eleven_tileset,0,0);
-
+        this.focus=this.add.sprite(38,35,'focus').setScale(.2);
         //add sprite
         this.x=this.physics.add.sprite(38,35,'X',0).setScale(.1);
-        // this.anims.create({
-        //     key:'jiggle',
-        //     frameRate:8,
-        //     repeat:-1,
-        //     frames: this.anims.generateFrameNumbers('slime',{
-        //         start:0,
-        //         end:1
-        //     })
-        // });
-        // this.slime.play('jiggle');
+
         this.x.body.setCollideWorldBounds(true);
         wallLayer.setCollisionByProperty({collides:true});
-        // treeLayer.setCollisionByProperty({collides:true});
         this.physics.add.collider(this.x,wallLayer);
-        // this.physics.add.collider(this.slime,treeLayer);
         // //cameras
         // this.cameras.main.setBounds(0,0, map.widthInPixels,map.heightInPixels);
         // this.cameras.main.startFollow(this.slime,true,0.25,0.25);
